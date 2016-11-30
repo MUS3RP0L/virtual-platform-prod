7cc596bcc12a1b69f1e186ed24afbd5b6a5c6449
aleguis@icloud.com <aleguis@icloud.com>
2016-11-30 18:28:01 -0400

    upgraded layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,34 @@
 
     @include('partials.htmlheader')
 
-    <body class="skin-green sidebar-mini fixed">
-    <!-- <body class="skin-green sidebar-mini fixed wysihtml5-supported sidebar-collapse"> -->
+    <!-- <body class="skin-green sidebar-mini fixed"> -->
+    <body class="skin-green sidebar-mini fixed wysihtml5-supported sidebar-collapse">
+
+    <div id="myModal-error" class="modal modal-danger fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Mensaje</h4>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        @foreach ($errors->all() as $error)
+                            <div><h4>{{ $error }}</h4></div>
+                        @endforeach
+                        <div><h4>{{Session::get('error')}}</h4></div>
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <div class="row text-center">
+                        <button type="button" class="btn btn-raised btn-default" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
         <div class="wrapper">
 
             @include('partials.mainheader')
