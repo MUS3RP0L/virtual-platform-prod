10c794bb02334fe6083ee77edf7d2d309c834fb5
aleguis@icloud.com <aleguis@icloud.com>
2016-12-29 18:47:10 -0400

    working on show voucher

--- resources/views/vouchers/show.blade.php
+++ resources/views/vouchers/show.blade.php
@@ -108,74 +108,60 @@
 
         <div class="col-md-6">
 
-        </div>
-    </div>
-    @if($voucher->payment_date)
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Despliegue</h3>
-                    </div>
-                    <div class="panel-body">
-                        <iframe src="{!! url('print_voucher/' . $voucher->id) !!}" width="99%" height="1200"></iframe>
+            @if($voucher->payment_date)
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Despliegue</h3>
+                            </div>
+                            <div class="panel-body">
+                                <iframe src="{!! url('print_voucher/' . $voucher->id) !!}" width="99%" height="1200"></iframe>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    @endif
-
-
-
-<div id="myModal-update" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Cobrar</h4>
-            </div>
-            <div class="modal-body">
-
-                {!! Form::model($voucher, ['method' => 'PATCH', 'route' => ['voucher.update', $voucher->id], 'class' => 'form-horizontal']) !!}
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                {!! Form::label('Total', 'Total', ['class' => 'col-md-5 control-label']) !!}
-                                <div class="col-md-6">
-                                    <h3>{!! $voucher->total !!}</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                {!! Form::label('received', 'Recibido', ['class' => 'col-md-5 control-label']) !!}
-                                <div class="col-md-3">
-                                    <input data-bind="value: received, valueUpdate: 'afterkeydown'" class="form-control" style="font-size:24px;"/>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                {!! Form::label('Cambio', 'Cambio', ['class' => 'col-md-5 control-label']) !!}
-                                <div class="col-md-6">
-                                    <h3><span data-bind="text: change()"></span></h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {!! Form::hidden('data', null, ['data-bind'=> 'value: ko.toJSON(model)']) !!}
-                    <div class="row text-center">
+            @else
+            {!! Form::model($voucher, ['method' => 'PATCH', 'route' => ['voucher.update', $voucher->id], 'class' => 'form-horizontal']) !!}
+                <div class="row">
+                    <div class="col-md-12">
                         <div class="form-group">
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-raised btn-success" data-toggle="tooltip" data-placement="bottom" data-original-title="Guardar">&nbsp;<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>&nbsp;</button>
+                            {!! Form::label('Total', 'Total', ['class' => 'col-md-5 control-label']) !!}
+                            <div class="col-md-6">
+                                <h3>{!! $voucher->total !!}</h3>
                             </div>
                         </div>
                     </div>
-                {!! Form::close() !!}
-            </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            {!! Form::label('received', 'Recibido', ['class' => 'col-md-5 control-label']) !!}
+                            <div class="col-md-3">
+                                <input data-bind="value: received, valueUpdate: 'afterkeydown'" class="form-control" style="font-size:24px;"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            {!! Form::label('Cambio', 'Cambio', ['class' => 'col-md-5 control-label']) !!}
+                            <div class="col-md-6">
+                                <h3><span data-bind="text: change()"></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {!! Form::hidden('data', null, ['data-bind'=> 'value: ko.toJSON(model)']) !!}
+                <div class="row text-center">
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-raised btn-success" data-toggle="tooltip" data-placement="bottom" data-original-title="Guardar">&nbsp;<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>&nbsp;</button>
+                        </div>
+                    </div>
+                </div>
+            {!! Form::close() !!}
+            @endif
+
         </div>
     </div>
-</div>
 
 @endsection
 
