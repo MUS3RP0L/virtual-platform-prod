3e3c0a104f7cd783a22bc725852553bc666c73f6
aleguis@icloud.com <aleguis@icloud.com>
2017-01-18 08:26:01 -0400

    bug fixes

--- app/EconomicComplementSubmittedDocument.php
+++ app/EconomicComplementSubmittedDocument.php
@@ -9,12 +9,15 @@ class EconomicComplementSubmittedDocument extends Model
     protected $table = 'eco_com_submitted_documents';
 
     protected $fillable = [
+
         'economic_complement_id',
         'eco_com_requirement_id',
         'reception_date',
         'status',
         'comment'
+
     ];
+
     protected $guarded = ['id'];
 
     public function economic_complement_requirement(){
@@ -23,6 +26,10 @@ class EconomicComplementSubmittedDocument extends Model
 
     public function economic_complement(){
         return $this->belongsTo('Muserpol\EconomicComplement');
+    }
+
+    public function scopeEconomicComplementIs($query, $id){
+        return $query->where('economic_complement_id', $id);
     }
 
 }
