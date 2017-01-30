d93fdbec56d9bda413ac41942605b597458bbf50
aleguis@icloud.com <aleguis@icloud.com>
2017-01-30 09:40:19 -0400

    updated sidevar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -15,12 +15,29 @@
             <a href="{!! url('direct_contribution') !!}"><i class='fa fa-fw fa-arrow-circle-down fa-lg' aria-hidden="true"></i>&nbsp; <span>Aportes Directos</span></a>
             </li>
 
-            <li {!! (Request::is('voucher') ? 'class=active' : '') !!}>
-            <a href="{!! url('voucher') !!}"><i class="fa fa-fw fa-usd fa-lg" aria-hidden="true"></i>&nbsp; <span>Cobros</a>
+
+            <li {!! (Request::is('retirement_fund') ? 'class=active' : '') !!}>
+            <a href="{!! url('retirement_fund') !!}">&nbsp;<i class="glyphicon glyphicon-piggy-bank" aria-hidden="true"></i>&nbsp; <span>Fondo de Retiro</span></a>
             </li>
 
             <li {!! (Request::is('economic_complement') ? 'class=active' : '') !!}>
             <a href="{!! url('economic_complement') !!}"><i class="fa fa-fw fa-puzzle-piece fa-lg" aria-hidden="true"></i>&nbsp; <span>Complemento Económico</span></a>
+            </li>
+
+            <li {!! (Request::is('mortuary') ? 'class=active' : '') !!}>
+            <a href="{!! url('mortuary') !!}"><i class="fa fa-fw fa-heartbeat fa-lg" aria-hidden="true"></i>&nbsp; <span>Cuota, Auxilio Mortuorio</span></a>
+            </li>
+
+            <li {!! (Request::is('voucher') ? 'class=active' : '') !!}>
+            <a href="{!! url('voucher') !!}"><i class="fa fa-fw fa-usd fa-lg" aria-hidden="true"></i>&nbsp; <span>Tesorería</a>
+            </li>
+
+            <li {!! (Request::is('budget') ? 'class=active' : '') !!}>
+            <a href="{!! url('budget') !!}"><i class="fa fa-fw fa-edit fa-lg" aria-hidden="true"></i>&nbsp; <span>Presupuesto</a>
+            </li>
+
+            <li {!! (Request::is('accounting') ? 'class=active' : '') !!}>
+            <a href="{!! url('accounting') !!}"><i class="fa fa-fw fa-book fa-lg" aria-hidden="true"></i>&nbsp; <span>Contabilidad</a>
             </li>
 
             <!-- <li class="treeview">
