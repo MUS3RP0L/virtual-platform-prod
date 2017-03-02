b9b6787330f581bac96d967e3ee846aa90e5377e
aleguis@icloud.com <aleguis@icloud.com>
2017-03-02 11:25:14 -0400

    working on applicant eco com

---
+++ resources/views/economic_complements/additional/affiliate_info.blade.php
@@ -0,0 +1,72 @@
+<div class="box box-warning">
+    <div class="box-header with-border">
+        <h3 class="box-title">Editar Afiliado</h3>
+    </div>
+    <div class="box-body">
+        <br>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <div class="col-md-12">
+                            {!! Form::label('identity_card_affi', 'Carnet de Identidad', ['class' => 'col-md-5 control-label']) !!}
+                        <div class="col-md-5">
+                            {!! Form::text('identity_card_affi', $affiliate->identity_card, ['class'=> 'form-control', 'required']) !!}
+                            <span class="help-block">Número de CI</span>
+                        </div>
+                            {!! Form::select('city_identity_card_id_lg', $cities_list_short, $affiliate->city_identity_card_id, ['class' => 'col-md-2 combobox form-control', 'required']) !!}
+                    </div>
+                </div>
+                <div class="form-group">
+                        {!! Form::label('last_name_affi', 'Apellido Paterno', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-6">
+                        {!! Form::text('last_name_affi', $affiliate->last_name, ['class'=> 'form-control', 'required', 'onkeyup' => 'this.value=this.value.toUpperCase()']) !!}
+                        <span class="help-block">Escriba el Apellido Paterno</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                        {!! Form::label('mothers_last_name_affi', 'Apellido Materno', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-6">
+                        {!! Form::text('mothers_last_name_affi', $affiliate->mothers_last_name, ['class'=> 'form-control', 'onkeyup' => 'this.value=this.value.toUpperCase()']) !!}
+                        <span class="help-block">Escriba el Apellido Materno</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                        {!! Form::label('first_name_affi', 'Primer Nombre', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-6">
+                        {!! Form::text('first_name_affi', $affiliate->first_name, ['class'=> 'form-control','required', 'onkeyup' => 'this.value=this.value.toUpperCase()']) !!}
+                        <span class="help-block">Escriba el  Primer Nombre</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                        {!! Form::label('second_name_affi', 'Segundo Nombre', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-6">
+                        {!! Form::text('second_name_affi', $affiliate->second_name, ['class'=> 'form-control', 'onkeyup' => 'this.value=this.value.toUpperCase()']) !!}
+                        <span class="help-block">Escriba el Segundo Nombre</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                @if ($affiliate->gender == 'F')
+                    <div class="form-group">
+                            {!! Form::label('surname_husband', 'Apellido de Esposo', ['class' => 'col-md-5 control-label']) !!}
+                        <div class="col-md-6">
+                            {!! Form::text('surname_husband', $affiliate->surname_husband, ['class'=> 'form-control', 'onkeyup' => 'this.value=this.value.toUpperCase()']) !!}
+                            <span class="help-block">Escriba el Apellido de Esposo (Opcional)</span>
+                        </div>
+                    </div>
+                @endif
+                <div class="form-group">
+                        {!! Form::label('birth_date', 'Fecha de Nacimiento', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-6">
+                        <div class="input-group">
+                            <input type="text" id="birth_date_mask" class="form-control" name="birth_date" value="{!! $eco_com_applicant->getEditBirthDate() !!}" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+                            <div class="input-group-addon">
+                                <span class="glyphicon glyphicon-calendar"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
