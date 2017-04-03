901272e6b314b209cba736ae0bcd6c93d4f11a4e
erickguis <aleguis@icloud.com>
2017-04-03 14:56:59 -0400

    migrate to postgres

--- database/migrations/2016_06_16_182627_sumar_aportesC_stored_procedure.php
+++ database/migrations/2016_06_16_182627_sumar_aportesC_stored_procedure.php
@@ -12,7 +12,7 @@ class SumarAportesCStoredProcedure extends Migration
      */
     public function up() {
 
-        DB::unprepared('CREATE PROCEDURE sum_contributionsC(IN month int, year int) BEGIN SELECT COUNT(DISTINCT(contributions.id)) count_id, SUM(contributions.base_wage) salary, SUM(contributions.seniority_bonus) seniority_bonus,SUM(contributions.study_bonus) study_bonus, SUM(contributions.position_bonus) position_bonus, SUM(contributions.border_bonus) border_bonus, SUM(contributions.east_bonus) east_bonus,SUM(contributions.public_security_bonus) public_security_bonus, SUM(contributions.gain) gain, SUM(contributions.quotable) quotable, SUM(contributions.total) total,SUM(contributions.retirement_fund) retirement_fund, SUM(contributions.mortuary_quota) mortuary_quota FROM contributions WHERE MONTH(contributions.month_year) = month and YEAR(contributions.month_year) = year and contributions.breakdown_id <> 5; END');
+        // DB::unprepared('CREATE PROCEDURE sum_contributionsC(IN month int, year int) BEGIN SELECT COUNT(DISTINCT(contributions.id)) count_id, SUM(contributions.base_wage) salary, SUM(contributions.seniority_bonus) seniority_bonus,SUM(contributions.study_bonus) study_bonus, SUM(contributions.position_bonus) position_bonus, SUM(contributions.border_bonus) border_bonus, SUM(contributions.east_bonus) east_bonus,SUM(contributions.public_security_bonus) public_security_bonus, SUM(contributions.gain) gain, SUM(contributions.quotable) quotable, SUM(contributions.total) total,SUM(contributions.retirement_fund) retirement_fund, SUM(contributions.mortuary_quota) mortuary_quota FROM contributions WHERE MONTH(contributions.month_year) = month and YEAR(contributions.month_year) = year and contributions.breakdown_id <> 5; END');
 
     }
 
@@ -23,8 +23,8 @@ class SumarAportesCStoredProcedure extends Migration
      */
     public function down() {
 
-        $sql = "DROP PROCEDURE IF EXISTS sum_contributionsC";
-        DB::connection()->getPdo()->exec($sql);
+        // $sql = "DROP PROCEDURE IF EXISTS sum_contributionsC";
+        // DB::connection()->getPdo()->exec($sql);
 
     }
 }
