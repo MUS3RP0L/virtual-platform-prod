7a1d6de7d520fc0fffb16b2a35d86f4fc86a477a
felixbvf <felixbvf@gmail.com>
2017-04-04 11:19:30 -0400

    Upgrade Store Procedure  C and B

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '5433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
