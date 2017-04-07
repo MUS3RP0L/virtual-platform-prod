1847d938614de0bac9bd7ef640f51d78062d9811
cechus <cechuscsc4@gmail.com>
2017-04-07 18:31:06 -0400

    dashboard for economic complement

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -16,6 +16,7 @@ use Muserpol\Spouse;
 use Muserpol\AffiliateState;
 use Muserpol\AffiliateType;
 use Muserpol\Contribution;
+use Muserpol\EconomicComplement;
 
 
 class DashboardController extends Controller
@@ -72,15 +73,18 @@ class DashboardController extends Controller
 			$affiliateType = AffiliateType::all();
 			$district = DB::table('units')->select(DB::raw('DISTINCT (units.district) as district'))->get();
 			$year = Carbon::now()->year;
+			
 			foreach ($affiliateState as $item) {
 				$TotalAffiliates = Affiliate::afibyState($item->id,$year)->first();
-				$Total_AffiliatebyState[$item->id] = $TotalAffiliates->total;
+				if ($TotalAffiliates->total) {
+					
+				$Total_AffiliatebyState[$item->name] = $TotalAffiliates->total;
+				}
 			}
-
 			foreach ($affiliateType as $item) {
 				$Afitype = Affiliate::afibyType($item->id,$year)->first();
 
-				$Total_AffiliatebyType[$item->id] = $Afitype->type;
+				$Total_AffiliatebyType[$item->name] = $Afitype->type;
 			}
 			//Total Contributions of last five year, disaggregated by year.
 			$lastContribution = DB::table('contributions')->orderBy('month_year', 'desc')->first();
@@ -115,7 +119,9 @@ class DashboardController extends Controller
 			foreach ($district as $item) {
 
 				$afiDistrict = Affiliate::afiDistrict($item->district, $year)->first();
-				$list_affiliateByDisctrict[$item->district] = $afiDistrict->district;
+				if ($afiDistrict->district) {
+					$list_affiliateByDisctrict[$item->district] = $afiDistrict->district;
+				}
 
 			}
 			$current_year=Carbon::now()->year;
@@ -128,7 +134,8 @@ class DashboardController extends Controller
 				->where(DB::raw('EXTRACT(MONTH FROM contributions.month_year)'), '=', $current_year)
 				->orderBy(DB::raw('EXTRACT(MONTH FROM contributions.month_year)'), 'asc')
 				->get();
-
+				//revisar
+				//dd($monthVoluntaryContribution);
 			 if($monthVoluntaryContribution)
 			{
 				foreach ($monthVoluntaryContribution as $item) {
@@ -149,18 +156,77 @@ class DashboardController extends Controller
 		$activities = Activity::orderBy('created_at', 'desc')->take(10)->get();
 		$totalAfi = $totalAfiServ + $totalAfiComi;
 
+
+		//for economic complement
+		$economic_complement=DB::table('economic_complements')
+			->select(DB::raw('COUNT(*) as quantity, EXTRACT(MONTH FROM economic_complements.reception_date) as month'))
+			//->groupBy(DB::raw('EXTRACT(MONTH FROM economic_complements.reception_date)'))
+			->groupBy('month')
+			->orderBy('month')
+			->get();
+		
+		foreach ($economic_complement as $item) {
+			$economic_complement_bar_labels[]= date("F", mktime(0, 0, 0, $item->month, 10));
+			$economic_complement_bar_datas[]= $item->quantity;
+		}
+		$economic_complement_bar= array($economic_complement_bar_labels, $economic_complement_bar_datas);
+		
+		// for economic complement types
+
+		$economic_complement_types=DB::table('eco_com_types')
+			->select(DB::raw('count(*) as quantity, eco_com_types.name as type_name'))
+			->join('eco_com_modalities','eco_com_types.id', '=', 'eco_com_modalities.eco_com_type_id')
+			->join('economic_complements','economic_complements.eco_com_modality_id','=','eco_com_modalities.id')
+			->groupBy('eco_com_types.name')
+			->get();
+		foreach ($economic_complement_types as $item) {
+			$economic_complement_pie_types_labels[]= $item->type_name;
+			$economic_complement_pie_types_datas[]= $item->quantity;
+		}
+		$economic_complement_pie_types=array($economic_complement_pie_types_labels,$economic_complement_pie_types_datas);
+		
+		//for economic complement  modalities types
+
+		$economic_complement_modalities_types=DB::table('eco_com_types')
+			->select(DB::raw('count(*) as quantity, eco_com_modalities.shortened'))
+			->join('eco_com_modalities','eco_com_types.id' ,'=', 'eco_com_modalities.eco_com_type_id')
+			->join('economic_complements','economic_complements.eco_com_modality_id','=','eco_com_modalities.id')
+			->groupBy('eco_com_modalities.shortened')
+			->get();
+		foreach ($economic_complement_modalities_types as $item) {
+			$economic_complement_modalities_types_datas[$item->shortened]= $item->quantity;
+		}
+		//for  economic complement for cities
+
+		$economic_complement_cities=DB::table('cities')
+			->select(DB::raw('count(*) as quantity, cities.name'))
+			->join('economic_complements','economic_complements.city_id','=','cities.id')
+			->groupBy('cities.name')
+			->orderBy('quantity')
+			->get();
+		
+		foreach ($economic_complement_cities as $item) {
+			$economic_complement_cities_data[$item->name]=$item->quantity;
+		}
+
 		$data = [
 			'activities' => $activities,
 			'totalAfiServ' => $totalAfiServ,
 			'totalAfiComi' => $totalAfiComi,
 			'totalAfi' => $totalAfi,
-			'Total_AffiliatebyState' => $Total_AffiliatebyState,
-			'Total_AffiliatebyType' => $Total_AffiliatebyType,
+			'Total_AffiliatebyState' => array(array_keys($Total_AffiliatebyState),array_values($Total_AffiliatebyState)),
+			'Total_AffiliatebyType' => array(array_keys($Total_AffiliatebyType),array_values($Total_AffiliatebyType)),
 			'totalContributionByYear' => $totalContributionByYear,
-			'list_affiliateByDisctrict' => $list_affiliateByDisctrict,
+			'list_affiliateByDisctrict' => array(array_keys($list_affiliateByDisctrict),array_values($list_affiliateByDisctrict)),
 			'total_voluntayContributionByMonth' => $total_voluntayContributionByMonth,
-			'current_year' => $current_year
+			'current_year' => $current_year,
+			'economic_complement_bar'=>$economic_complement_bar,
+			'economic_complement_pie_types'=>$economic_complement_pie_types,
+			'economic_complement_modalities_types'=>array(array_keys($economic_complement_modalities_types_datas),array_values($economic_complement_modalities_types_datas)),
+			'economic_complement_cities'=>array(array_keys($economic_complement_cities_data),array_values($economic_complement_cities_data)),
+
 		];
+		//dd(array(array_keys($economic_complement_modalities_types),array_values($economic_complement_modalities_types)));
 	
 				return view('dashboard.index', $data);
 
