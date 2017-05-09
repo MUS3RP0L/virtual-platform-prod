70aa230689dff23e3cbf1318d21909ff4ff4c4f7
felixbvf <felixbvf@gmail.com>
2017-05-09 19:20:40 -0400

    Upgrade migration create_views

--- database/migrations/2017_05_11_181957_create_views.php
+++ database/migrations/2017_05_11_181957_create_views.php
@@ -26,28 +26,30 @@ class CreateViews extends Migration
                  LEFT JOIN eco_com_types ON eco_com_modalities.eco_com_type_id = eco_com_types.id
                  WHERE (economic_complements.affiliate_id IN ( SELECT economic_complements_1.affiliate_id         FROM economic_complements economic_complements_1
                   GROUP BY economic_complements_1.affiliate_id
-                 HAVING count(economic_complements_1.affiliate_id) > 1)); CREATE OR REPLACE VIEW public.v_inclusion AS
-                          SELECT economic_complements.id,
-                             eco_com_applicants.identity_card,
-                             cities.id AS city_id,
-                             cities.name AS city,
-                             eco_com_types.id AS type_id,
-                             eco_com_types.name AS type,
-                             degrees.id AS degree_id,
-                             degrees.shortened AS degree,
-                             economic_complements.semester,
-                             economic_complements.year AS year1
-                            FROM eco_com_applicants
-                              LEFT JOIN economic_complements ON eco_com_applicants.economic_complement_id = economic_complements.id
-                              LEFT JOIN affiliates ON economic_complements.affiliate_id = affiliates.id
-                              LEFT JOIN degrees ON affiliates.degree_id = degrees.id
-                              LEFT JOIN cities ON economic_complements.city_id = cities.id
-                              LEFT JOIN eco_com_modalities ON economic_complements.eco_com_modality_id = eco_com_modalities.id
-                              LEFT JOIN eco_com_types ON eco_com_modalities.eco_com_type_id = eco_com_types.id
-                           WHERE (economic_complements.affiliate_id IN ( SELECT economic_complements_1.affiliate_id
-                                    FROM economic_complements economic_complements_1
-                                   GROUP BY economic_complements_1.affiliate_id
-                                  HAVING count(economic_complements_1.affiliate_id) = 1))');
+                 HAVING count(economic_complements_1.affiliate_id) > 1));');
+
+        DB::unprepared('CREATE OR REPLACE VIEW public.v_inclusion AS
+                 SELECT economic_complements.id,
+                    eco_com_applicants.identity_card,
+                    cities.id AS city_id,
+                    cities.name AS city,
+                    eco_com_types.id AS type_id,
+                    eco_com_types.name AS type,
+                    degrees.id AS degree_id,
+                    degrees.shortened AS degree,
+                    economic_complements.semester,
+                    economic_complements.year AS year1
+                   FROM eco_com_applicants
+                     LEFT JOIN economic_complements ON eco_com_applicants.economic_complement_id = economic_complements.id
+                     LEFT JOIN affiliates ON economic_complements.affiliate_id = affiliates.id
+                     LEFT JOIN degrees ON affiliates.degree_id = degrees.id
+                     LEFT JOIN cities ON economic_complements.city_id = cities.id
+                     LEFT JOIN eco_com_modalities ON economic_complements.eco_com_modality_id = eco_com_modalities.id
+                     LEFT JOIN eco_com_types ON eco_com_modalities.eco_com_type_id = eco_com_types.id
+                  WHERE (economic_complements.affiliate_id IN ( SELECT economic_complements_1.affiliate_id
+                           FROM economic_complements economic_complements_1
+                          GROUP BY economic_complements_1.affiliate_id
+                         HAVING count(economic_complements_1.affiliate_id) = 1));');
 
     }
 
