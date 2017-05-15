c3320f6852d0972936857ab55f15bed904b8a326
Ale Guis F <aleguis@icloud.com>
2017-05-15 15:47:41 -0400

    working on ret fun

--- database/migrations/2017_05_06_131515_create_platform_tables.php
+++ database/migrations/2017_05_06_131515_create_platform_tables.php
@@ -556,6 +556,7 @@ class CreatePlatformTables extends Migration
             $table->string('shortened');
             $table->foreign('eco_com_type_id')->references('id')->on('eco_com_types');
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('eco_com_submitted_documents', function (Blueprint $table) {
@@ -607,6 +608,109 @@ class CreatePlatformTables extends Migration
             $table->string('phone_number')->nullable();
             $table->string('cell_phone_number')->nullable();
             $table->foreign('economic_complement_id')->references('id')->on('economic_complements')->onDelete('cascade');
+            $table->foreign('city_identity_card_id')->references('id')->on('cities');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('ret_fun_modalities', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('shortened');
+            $table->timestamps();
+        });
+
+        Schema::create('retirement_funds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('affiliate_id')->unsigned();
+            $table->bigInteger('ret_fun_modality_id')->unsigned()->nullable();
+            $table->bigInteger('city_id')->unsigned()->nullable();
+            $table->string('code')->unique()->required();
+            $table->date('reception_date')->nullable();
+            $table->date('check_file_date')->nullable();
+            $table->date('qualification_date')->nullable();
+            $table->date('legal_assessment_date')->nullable();
+            $table->date('anticipation_start_date')->nullable();
+            $table->date('anticipation_end_date')->nullable();
+            $table->date('recognized_start_date')->nullable();
+            $table->date('recognized_end_date')->nullable();
+            $table->decimal('total_quotable', 13, 2);
+            $table->decimal('total_additional_quotable', 13, 2);
+            $table->decimal('subtotal', 13, 2);
+            $table->decimal('performance', 13, 2);
+            $table->decimal('total', 13, 2);
+            $table->string('comment'); 
+            $table->foreign('affiliate_id')->references('id')->on('affiliates')->onDelete('cascade');
+            $table->foreign('ret_fun_modality_id')->references('id')->on('ret_fun_modalities');
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('ret_fun_requirements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('ret_fun_modality_id')->unsigned();
+            $table->string('name');
+            $table->string('shortened');
+            $table->foreign('ret_fun_modality_id')->references('id')->on('ret_fun_modalities');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('ret_fun_submitted_documents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('retirement_fund_id')->unsigned();
+            $table->bigInteger('ret_fun_requirement_id')->unsigned();
+            $table->date('reception_date');
+            $table->boolean('status')->default(0);
+            $table->string('comment')->nullable();
+            $table->foreign('retirement_fund_id')->references('id')->on('retirement_funds')->onDelete('cascade');
+            $table->foreign('ret_fun_requirement_id')->references('id')->on('ret_fun_requirements');
+            $table->unique(['retirement_fund_id', 'ret_fun_requirement_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('ret_fun_antecedent_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('shortened');
+            $table->timestamps();
+        });
+
+        Schema::create('ret_fun_antecedents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('retirement_fund_id')->unsigned();
+            $table->bigInteger('ret_fun_antecedent_file_id')->unsigned();  
+            $table->boolean('status')->default(0);
+            $table->date('reception_date')->nullable();
+            $table->string('code')->nullable();
+            $table->foreign('retirement_fund_id')->references('id')->on('retirement_funds')->onDelete('cascade');
+            $table->foreign('ret_fun_antecedent_file_id')->references('id')->on('ret_fun_antecedent_files');
+            $table->unique(['retirement_fund_id', 'ret_fun_antecedent_file_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('ret_fun_applicants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('retirement_fund_id')->unsigned();
+            $table->bigInteger('city_identity_card_id')->unsigned()->nullable();
+            $table->string('identity_card')->required();
+            $table->string('last_name')->nullable();
+            $table->string('mothers_last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('second_name')->nullable();
+            $table->string('surname_husband')->nullable();
+            $table->string('kinship')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', ['M', 'F']);
+            $table->enum('civil_status', ['C', 'S', 'V', 'D'])->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('cell_phone_number')->nullable();
+            $table->string('home_address')->nullable();
+            $table->string('work_address')->nullable();
+            $table->foreign('retirement_fund_id')->references('id')->on('retirement_funds')->onDelete('cascade');
             $table->foreign('city_identity_card_id')->references('id')->on('cities');
             $table->timestamps();
             $table->softDeletes();
