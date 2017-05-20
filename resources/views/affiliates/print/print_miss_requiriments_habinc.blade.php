6525efe0a4fae7c3e385a1a87ea36ae264a7c4c8
qaje <javierpal3232@gmail.com>
2017-05-20 17:31:47 +0200

    print requiriment inclusion

---
+++ resources/views/affiliates/print/print_miss_requiriments_habinc.blade.php
@@ -0,0 +1,92 @@
+@extends('globalprint.print')
+@section('title2')
+<h3>(Página 1/2)</h3>
+@endsection
+@section('content')
+
+<table>
+  <p align="justify">
+      En fecha ……../…..…./.…., a horas…..…………se <b>NOTIFICA</b> en forma personal al Sr.(a)…………………………………………..………………….con <b>CARTA DE NOTIFICACIÓN de la Unidad de Complemento Económico N°</b> …………de fecha….…/………/……., quien recibió en mano propia el original de dicho documento.
+  </p>
+</table>
+
+  <table align="center" width="10" >
+    <thead>
+      <tr>
+        <th align="center"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="grand service" style="text-align:center;width: 10%;height: 10%"><p align="center">FIRMA DEL NOTIFICADO.</p></td>
+      </tr>
+        <tr>
+        <td class="grand service" style="text-align:center;width: 10%;height: 10%"><p align="center">ACLARACIÓN DE FIRMA.</p></td>
+      </tr>
+    </tbody>
+  </table>
+<footer>
+  PLATAFORMA VIRUTAL - MUTUAL DE SERVICIOS AL POLICIA
+</footer>
+<div class="page-break"></div>
+<header class="clearfix">
+  <table class="tableh">
+    <tr>
+      <th style="width: 25%;border: 0px;">
+        <div id="logo">
+          <img src="img/logo.jpg">
+        </div>
+      </th>
+      <th style="width: 50%;border: 0px">
+        <h4><b>MUTUAL DE SERVICIOS AL POLICIA<br>
+          {!! $header1 !!} <br> {!! $header2 !!}
+        </b></h4>
+      </th>
+      <th style="width: 25%;border: 0px">
+        <div id="logo2">
+          <img src="img/escudo.jpg">
+        </div>
+      </th>
+    </tr>
+  </table>
+  <h1>
+    <b>
+      {{ $title }}<br>
+      <h3>(Página 2/2)</h3>
+    </b>
+  </h1>
+</header>
+  <table>
+      <p align="right"> La Paz, Elija un elemento. de Elija un elemento. de 2017
+      <br>CITE: UCE – EGSB /2017
+      </p>
+  </table>
+  <table>
+      <p align="left">Señor (a): <br>
+      Haga clic aquí para escribir texto.……………………………………………..<br>
+      Presente.-
+       </p>
+  </table>
+         <p align="right"><b>REF.- <ins> CARTA DE NOTIFICACIÓN </ins></b></p>
+        <p align="justify">De nuestra consideracion.</p>
+         <p>
+              De la revisión efectuada por el Área Técnica de la Unidad de otorgación del Complemento Económico, <b>se comunica a usted  que no cumple con los requisitos establecidos según Reglamento vigente, para acceder al Beneficio del Complemento Económico correspondiente al  Elija un elemento. Semestre de la Gestión Elija un elemento..</b>
+        </p>
+    <p align="justify"><b>
+      Y en estricto cumplimiento al Decreto Supremo Nº 1446</b>, de fecha 19 de diciembre de 2012, que crea la Mutual de Servicios al Policía ¨MUSERPOL¨, establece en su Artículo 3º.- (Funciones y fines). La MUSERPOL tiene las siguientes funciones y fines: inc. <b>5) Pagar el Complemento Económico al sector pasivo de la Policía Boliviana conforme a Reglamento</b>.
+    <p align="justify">
+      Asimismo, se halla enmarcado en las normas legales vigentes, el <b>Reglamento del Complemento Económico de 2016</b>, aprobado mediante Resolución de Directorio Nº 36/2016 de fecha 09 de diciembre de 2016, que norma en su <b>ARTÍCULO 22° (Requisitos para la Inclusión y Calificación) punto 4, refiere lo siguiente:</b></p>
+    </p>
+    <p align="justify"><b> 
+      En caso de que el beneficiario (a) habitual deje de percibir el beneficio del Complemento Económico por una Gestión o más, para ser habilitado en la siguiente gestión, deberá presentar los requisitos establecidos en el Art. 17° del Reglamento como nuevo beneficiario (a) (Inclusión).
+    </b></p>
+    <p align="justify">
+      <b>En observancia al Artículo 28° (Observación documental).</b> I. En los casos que se haya presentado solicitud de Pago del Complemento Económico y se evidencie alguna observación o inconsistencia de índole documental, será comunicado mediante carta de notificación y <b>tendrá un plazo de 90 días calendario, para gestionar la documentación faltante.</b> En caso de no presentar la documentación faltante y prescrito el plazo señalado, no podrá acceder al Pago del Complemento Económico del semestre o semestres solicitados. En caso de ser representada dicha carta, se procederá con la emisión de un Informe Técnico – Legal.
+    </p>
+    <p align="justify">
+          Por lo que usted no podrá acceder al Beneficio del Complemento Económico, correspondiente al Elija un elemento. Semestre de la gestión Elija un elemento., en tanto no presente  los requisitos establecidos según el Art. 17° del Reglamento.<br>
+          Sin otro motivo en particular, saludo a usted.<br>
+          Atentamente.
+    </p>
+</div>
+@endsection
