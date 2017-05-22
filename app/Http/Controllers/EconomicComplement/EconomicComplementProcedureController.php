677456ffe910add742b455acafc7034f27df494e
Ale Guis F <aleguis@icloud.com>
2017-05-22 09:33:18 -0400

    eco com procedure controller added

---
+++ app/Http/Controllers/EconomicComplement/EconomicComplementProcedureController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Muserpol\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Muserpol\Http\Requests;
+use Muserpol\Http\Controllers\Controller;
+
+use Auth;
+use Validator;
+use Session;
+use Datatables;
+use Carbon\Carbon;
+use Muserpol\Helper\Util;
+
+use Muserpol\ComplementaryFactor;
+use Muserpol\Hierarchy;
+
+class EconomicComplementProcedureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
