1f555736357c27d6a1cca4cd64fd668b352e13d3
Ale Guis F <aleguis@icloud.com>
2017-05-22 08:28:34 -0400

    eco com procedure model added

---
+++ app/EconomicComplementProcedure.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Muserpol;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EconomicComplementProcedure extends Model
+{
+    protected $table = 'eco_com_procedures';
+    
+    protected $fillable = [
+  		'year',
+  		'semester',
+  		'normal_start_date',
+		'normal_end_date',
+		'lagging_start_date',
+		'lagging_end_date',
+		'additional_start_date',
+		'additional_end_date'
+  	];
+
+    protected $guarded = ['id'];
+
+}
