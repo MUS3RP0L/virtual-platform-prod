afea84c5f07974743e3bd80eff2e413a9c5e52a0
qaje <javierpal3232@gmail.com>
2017-05-22 14:49:46 -0700

    legalaction & correct & out of 60

---
+++ resources/views/affiliates/print/out_of_time90.blade.php
@@ -0,0 +1,99 @@
+@extends('globalprint.print')
+@section('title2')
+<h3>(Página 1/2)</h3>
+@endsection
+@section('content')
+
+<table>
+  <p align="justify">
+      En fecha ……../…..…./.…., a horas…..…………se <b>NOTIFICA</b> en forma personal al Sr.(a)…………………………………………..………………….con <b>CARTA DE NOTIFICACIÓN de la Unidad de Complemento Económico N°</b> …………de fecha….…/………/……., quien recibió en mano propia el original de dicho documento.
+  </p>
+</table>
+
+  <table align="center" width="10" >
+    <thead>
+      <tr>
+        <th align="center"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="grand service" style="text-align:center;width: 10%;height: 10%"><p align="center">FIRMA DEL NOTIFICADO.</p></td>
+      </tr>
+        <tr>
+        <td class="grand service" style="text-align:center;width: 10%;height: 10%"><p align="center">ACLARACIÓN DE FIRMA.</p></td>
+      </tr>
+    </tbody>
+  </table>
+<footer>
+  PLATAFORMA VIRUTAL - MUTUAL DE SERVICIOS AL POLICIA
+</footer>
+<div class="page-break"></div>
+<header class="clearfix">
+  <table class="tableh">
+    <tr>
+      <th style="width: 25%;border: 0px;">
+        <div id="logo">
+          <img src="img/logo.jpg">
+        </div>
+      </th>
+      <th style="width: 50%;border: 0px">
+        <h4><b>MUTUAL DE SERVICIOS AL POLICIA<br>
+          {!! $header1 !!} <br> {!! $header2 !!}
+        </b></h4>
+      </th>
+      <th style="width: 25%;border: 0px">
+        <div id="logo2">
+          <img src="img/escudo.jpg">
+        </div>
+      </th>
+    </tr>
+  </table>
+  <h1>
+    <b>
+      {{ $title }}<br>
+      <h3>(Página 2/2)</h3>
+    </b>
+  </h1>
+</header>
+  <table>
+      <p align="right"> La Paz, Elija un elemento. de Elija un elemento. de 2017
+      <br>CITE: UCE – EGSB /2017
+      </p>
+  </table>
+  <table>
+      <p align="left">Señor (a): <br>
+      Haga clic aquí para escribir texto.……………………………………………..<br>
+      Presente.-
+       </p>
+  </table>
+         <p align="right"><b>REF.- <ins> CARTA DE NOTIFICACIÓN </ins></b></p>
+        <p align="justify">De nuestra consideracion.</p>
+         <p>
+              Mediante la presente y en atención al reclamo realizado por su persona, se comunica que de la recalificación realizada por el Área Técnica de la Unidad de Otorgación del Complemento Económico, <b>el importe pagado de Bs. ... es el correcto.</b>
+        </p>
+    <p align="justify"><b>
+      Y en estricto cumplimiento al Decreto Supremo Nº 1446</b>, de fecha 19 de diciembre de 2012, que crea la Mutual de Servicios al Policía ¨MUSERPOL¨, establece en su Artículo 3º.- (Funciones y fines). La MUSERPOL tiene las siguientes funciones y fines: inc. 5) <b>Pagar el Complemento Económico al sector pasivo de la Policía Boliviana conforme a Reglamento. </b>
+      </p>
+    <p align="justify">
+      Asimismo, se halla enmarcado en las normas legales vigentes, el <b>Reglamento del Complemento Económico de 2016</b>, aprobado mediante Resolución de Directorio Nº 36/2016 de fecha 09 de diciembre de 2016, que norma en su <b>ARTÍCULO 26° (Reclamos). </b>I. El beneficiario podrá presentar reclamo mediante nota escrita, dirigido a la Dirección General Ejecutiva, dicha nota podrá ser presentada en la Oficina Central de La Paz – MUSERPOL o en las Regionales; acompañando toda la documentación de respaldo, señalando domicilio y número telefónico, desde el 1er. día hábil posterior a realizado el Pago por la  Entidad Bancaria o Financiera y/o recojo del cheque de Pago del Complemento Económico, en los siguientes casos:
+          <ul type="a">
+            <li> Monto cobrado del Complemento Económico.</li>
+            <li> Error en el grado y/o categoría.</li>
+          </ul>
+      </p>
+      <p align="justify">
+          En los casos descritos anteriormente, para los beneficiarios o terceros acreditados que cobren el Complemento Económico, contarán con un plazo de 10 días hábiles para efectuar el reclamo, que correrá a partir desde el 1er. día hábil posterior a realizado el Pago por la Entidad Bancaria o Financiera y/o recojo del cheque de Pago del Complemento Económico.
+      </p>
+      <p align="justify">
+          Una vez recepcionado el reclamo escrito, se dará respuesta a la (el) solicitante, mediante carta de notificación, en caso de ser representado, la Unidad de Complemento Económico, efectuará un análisis técnico-legal de la documentación presentada por el beneficiario, asimismo se podrá solicitar complementaciones e información adicional a la Autoridad de Fiscalización y Control de Pensiones y Seguros, Servicio Nacional de Sistema de Reparto, Comando General de la Policía Boliviana y otras entidades requeridas. Una vez revisada la documentación presentada, se procederá a la emisión de un informe técnico-legal dentro del plazo de quince (15) días hábiles desde el día hábil siguiente a la presentación de reclamo.
+      </p>
+      <p align="justify">
+          Por lo que de la recalificación y revisión realizada con referencia al monto de pago del Beneficio del Complemento Económico, correspondiente al ... Semestre de la Gestión ... , por el importe de Bs ... . Haga clic aquí para escribir texto., <b>no existe error en el monto pagado.</b> Asimismo, comunicar a su persona que el monto individual del Complemento Económico es <b>variable</b>, determinado semestralmente en base a un Estudio Técnico Financiero y Reglamentación, aprobado por el Directorio de la MUSERPOL, en función a las transferencias determinadas por Ley para el pago del Complemento Económico.
+      </p>
+      <p align="justify">
+          Sin otro motivo en particular, saludo a usted.<br>
+          Atentamente.
+    </p>
+</div>
+@endsection
