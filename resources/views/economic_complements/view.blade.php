ec9b866d221300295db313a67eb81724f893f79b
Ale Guis F <aleguis@icloud.com>
2017-05-23 00:30:31 -0400

    bug fixes

--- resources/views/economic_complements/view.blade.php
+++ resources/views/economic_complements/view.blade.php
@@ -13,17 +13,13 @@
 	            </a>
 	        </div>
 		</div>
-		@can('eco_com_review')
-			@if($economic_complement->eco_com_state_id < 2)
-				<div class="col-md-2 text-right">
-			        <div class="btn-group" data-toggle="tooltip" data-placement="top" data-original-title="Confirmar" style="margin:0px;">
-		                <a href="" data-target="#myModal-confirm" class="btn btn-raised btn-warning dropdown-toggle enabled" data-toggle="modal">
-		                    &nbsp;<span class="glyphicon glyphicon-ok"></span>&nbsp;
-		                </a>
-		            </div>
-				</div>
-			@endif
-		@endcan
+		<div class="col-md-2 text-right">
+	        <div class="btn-group" data-toggle="tooltip" data-placement="top" data-original-title="Confirmar" style="margin:0px;">
+                <a href="" data-target="#myModal-confirm" class="btn btn-raised btn-warning dropdown-toggle enabled" data-toggle="modal">
+                    &nbsp;<span class="glyphicon glyphicon-ok"></span>&nbsp;
+                </a>
+            </div>
+		</div>
 	</div>
 
 @endsection
