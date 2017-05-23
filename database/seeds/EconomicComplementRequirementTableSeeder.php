51dc8946a161d491717d4c08113a3f17c646d136
Ale Guis F <aleguis@icloud.com>
2017-05-23 11:08:19 -0400

    bug fies

--- database/seeds/EconomicComplementRequirementTableSeeder.php
+++ database/seeds/EconomicComplementRequirementTableSeeder.php
@@ -21,6 +21,7 @@ class EconomicComplementRequirementTableSeeder extends Seeder
 
     private function createEconomicComplementRequirements()
     {
+        $statuses = [
 
             ['eco_com_type_id' => '1', 'name' => 'Fotocopia de Boleta de Renta', 'shortened' => 'Fotocopia de Boleta de Renta'],
             ['eco_com_type_id' => '1', 'name' => 'Fotocopia de Cédula de Identidad', 'shortened' => 'Fotocopia de Cédula de Identidad'],
@@ -29,8 +30,8 @@ class EconomicComplementRequirementTableSeeder extends Seeder
             ['eco_com_type_id' => '1', 'name' => 'Fotocopia de la Resolución de SENASIR o contrato de la AFP o de la Aseguradora; excepcionalmente se podrá admitir fotocopia legalizada de la Resolución otorgada de renta de vejez', 'shortened' => 'Fotocopia de Resolución de SENASIR o contrato de la AFP o de la Aseguradora'],
 
             ['eco_com_type_id' => '2', 'name' => 'Fotocopia de Boleta de Renta', 'shortened' => 'Fotocopia de Boleta de Renta'],
-            ['eco_com_type_id' => '2', 'name' => 'Fotocopia de Cédula de Identidad del derechohabiente', 'shortened' => 'Fotocopia de Cédula de Identidad del derechohabiente'],
             ['eco_com_type_id' => '2', 'name' => 'Fotocopia de Cédula de Identidad del causahabiente ', 'shortened' => 'Fotocopia de Cédula de Identidad del causahabiente'],
+            ['eco_com_type_id' => '2', 'name' => 'Fotocopia de Cédula de Identidad del derechohabiente', 'shortened' => 'Fotocopia de Cédula de Identidad del derechohabiente'],
             ['eco_com_type_id' => '2', 'name' => 'Certificado Original de Defunción del causahabiente', 'shortened' => 'Certificado Original de Defunción del causahabiente'],
             ['eco_com_type_id' => '2', 'name' => 'Fotocopia de la Resolución de SENASIR o contrato de la AFP o de la Aseguradora; excepcionalmente se podrá admitir fotocopia legalizada de la Resolución otorgada de renta de vejez', 'shortened' => 'Fotocopia de Resolución de SENASIR o contrato de la AFP o de la Aseguradora'],
             ['eco_com_type_id' => '2', 'name' => 'Fotocopia legible del Memorándum de Agradecimiento de servicios o Certificación de Trabajo, emitidos por el Comando General de la Policía Boliviana.', 'shortened' => 'Fotocopia de Memorándum de Agradecimiento de servicios o Certificación de Trabajo'],
@@ -38,8 +39,8 @@ class EconomicComplementRequirementTableSeeder extends Seeder
             ['eco_com_type_id' => '2', 'name' => 'Certificación original de verificación de partidas matrimoniales emitida por el Servicio de Registro Civil – SERECI', 'shortened' => 'Certificación original de verificación de partidas matrimoniales'],
 
             ['eco_com_type_id' => '3', 'name' => 'Fotocopia de Boleta de Renta', 'shortened' => 'Fotocopia de Boleta de Renta'],
-            ['eco_com_type_id' => '3', 'name' => 'Fotocopia de Cédula de Identidad del derechohabiente', 'shortened' => 'Fotocopia de Cédula de Identidad del derechohabiente'],
             ['eco_com_type_id' => '3', 'name' => 'Fotocopia de Cédula de Identidad del causahabiente ', 'shortened' => 'Fotocopia de Cédula de Identidad del causahabiente'],
+            ['eco_com_type_id' => '3', 'name' => 'Fotocopia de Cédula de Identidad del derechohabiente', 'shortened' => 'Fotocopia de Cédula de Identidad del derechohabiente'],
             ['eco_com_type_id' => '3', 'name' => 'Certificado Original de Defunción del causahabiente', 'shortened' => 'Certificado Original de Defunción del causahabiente'],
             ['eco_com_type_id' => '3', 'name' => 'Fotocopia de la Resolución de SENASIR o contrato de la AFP o de la Aseguradora; excepcionalmente se podrá admitir fotocopia legalizada de la Resolución otorgada de renta de vejez', 'shortened' => 'Fotocopia de Resolución de SENASIR o contrato de la AFP o de la Aseguradora'],
             ['eco_com_type_id' => '3', 'name' => 'Fotocopia legalizada de Resolución judicial de declaración de tutela', 'shortened' => 'Fotocopia legalizada de Resolución judicial de declaración de tutela'],
