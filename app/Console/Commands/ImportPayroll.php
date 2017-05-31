c95348f34450d73434e3e9ab1dd03c834981d813
NEXUS\Felix <felixbvf@gmail.com>
2017-05-31 11:25:03 -0600

    bug fixes command ImportPayroll

--- app/Console/Commands/ImportPayroll.php
+++ app/Console/Commands/ImportPayroll.php
@@ -68,7 +68,7 @@ class ImportPayroll extends Command
                         ini_set('max_input_time', '-1');
                         set_time_limit('-1');
 
-                        if (!isset($result->car) or !isset($result->pat) or !isset($result->mat) or !isset($result->nom) or
+                        /*if (!isset($result->car) or !isset($result->pat) or !isset($result->mat) or !isset($result->nom) or
                             !isset($result->nom2) or !isset($result->apes) or !isset($result->eciv) or !isset($result->sex) or
                             !isset($result->nac) or !isset($result->ing) or !isset($result->mes) or !isset($result->a_o) or
                             !isset($result->uni) or !isset($result->desg) or !isset($result->niv) or !isset($result->gra) or
@@ -80,50 +80,52 @@ class ImportPayroll extends Command
                             !isset($result->nua) or !isset($result->mus)) {
                             $this->error('Missing columns in the file!');
                             exit();
-                        }
+                        }*/
 
                         switch ($FolderName) {
 
                             case 'c1':
                                 $first_name = Util::FirstName($result->nom);
                                 $second_name = Util::SecondName($result->nom);
-                                $birth_date = Util::dateDDMMAA($result->nac);
-                                $date_entry = Util::dateDDMMAA($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::dateDDMMAA($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::dateDDMMAA($result->ing));
                             break;
 
                             case 'c2':
                                 $first_name = Util::FirstName($result->nom);
                                 $second_name = Util::SecondName($result->nom);
-                                $birth_date = Util::dateAAMMDD($result->nac);
-                                $date_entry = Util::dateAAMMDD($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::dateAAMMDD($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::dateAAMMDD($result->ing));
                             break;
 
                             case 'c3':
                                 $first_name = Util::FirstName($result->nom);
                                 $second_name = Util::SecondName($result->nom);
-                                $birth_date = Util::dateAADDMM($result->nac);
-                                $date_entry = Util::dateAADDMM($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::dateAADDMM($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::dateAADDMM($result->ing));
+
                             break;
 
                             case 'c4':
                                 $first_name = $result->nom;
                                 $second_name = $result->nom2;
-                                $birth_date = Util::dateAAMMDD($result->nac);
-                                $date_entry = Util::dateAAMMDD($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::dateAAMMDD($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::dateAAMMDD($result->ing));
+
                             break;
 
                             case 'c5':
                                 $first_name = $result->nom;
                                 $second_name = $result->nom2;
-                                $birth_date = Util::dateDDMMAA($result->nac);
-                                $date_entry = Util::dateDDMMAA($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::dateDDMMAA($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::dateDDMMAA($result->ing));
                             break;
 
                             default:
                                 $first_name = $result->nom;
                                 $second_name = $result->nom2;
-                                $birth_date = Util::date($result->nac);
-                                $date_entry = Util::date($result->ing);
+                                $birth_date = ($result->nac = '000000' ? null : Util::date($result->nac));
+                                $date_entry = ($result->ing = '000000' ? null : Util::date($result->ing));
                         }
                         $Date = Util::zero($result->mes) . "-" . Util::formatYear($result->a_o);
 
@@ -216,7 +218,7 @@ class ImportPayroll extends Command
                         $affiliate->first_name = Util::replaceCharacter($first_name);
                         $affiliate->second_name = Util::replaceCharacter($second_name);
                         $affiliate->surname_husband = Util::replaceCharacter($result->apes);
-                        $affiliate->civil_status = $result->eciv;
+                        $affiliate->civil_status = 'S';
                         $affiliate->nua = $result->nua;
                         $affiliate->afp = Util::getAfp($result->afp);
                         $affiliate->item = $result->item;
