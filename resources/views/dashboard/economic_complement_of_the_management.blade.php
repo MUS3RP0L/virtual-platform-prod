b28b7227e609ef7e3657e8277a4a8a7ecb70baf1
cechus <cechuscsc4@gmail.com>
2017-07-03 11:39:16 -0400

    bug fixes on dashboard

--- resources/views/dashboard/economic_complement_of_the_management.blade.php
+++ resources/views/dashboard/economic_complement_of_the_management.blade.php
@@ -1,6 +1,6 @@
 <div class="box box-success">
 	<div class="box-header with-border">
-		<h3 class="box-title">Total complemento economico de la Gestión {!! $current_year !!}</h3>
+		<h3 class="box-title">Total complemento economico del {!! $last_economic_complement->semester !!} semestre del {!! $last_year !!}</h3>
 	</div>
 	<div class="box-body" style="width: 95%">
 		<canvas id="bar-semestre" width="450px" height="150"></canvas>
