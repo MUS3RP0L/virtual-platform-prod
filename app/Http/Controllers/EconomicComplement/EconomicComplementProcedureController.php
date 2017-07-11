86a80b4948988ce67b555d449290fcfd5eee4710
cechus <cechuscsc4@gmail.com>
2017-07-11 14:48:35 -0400

    working on crud procedure

--- app/Http/Controllers/EconomicComplement/EconomicComplementProcedureController.php
+++ app/Http/Controllers/EconomicComplement/EconomicComplementProcedureController.php
@@ -31,8 +31,15 @@ class EconomicComplementProcedureController extends Controller
         $procedures = EconomicComplementProcedure::select(['id', 'year', 'semester', 'normal_start_date', 'normal_end_date', 'lagging_start_date', 'lagging_end_date',  'additional_start_date', 'additional_end_date']);
         return Datatables::of($procedures)
                ->editColumn('year', function ($procedure) { return Util::getYear($procedure->year); })
-            ->make(true);
-
+               ->addColumn('action', function ($procedure) { return
+                   '<div class="btn-group" style="margin:-3px 0;">
+                   <a href="" data-procedure_id="'.$procedure->id.'" class="btn btn-primary btn-raised btn-sm editProcedure" data-toggle="modal" data-target="#modalEditProcedure">&nbsp;&nbsp;<i class="fa fa-pencil"></i>&nbsp;&nbsp;</a>
+                   <a href="" class="btn btn-primary btn-raised btn-sm dropdown-toggle" data-toggle="dropdown">&nbsp;<span class="caret"></span>&nbsp;</a>
+                   <ul class="dropdown-menu">
+                       <li><a href="economic_complement_procedure/ '.$procedure->id.'/edit " style="padding:3px 10px;"><i class="fa fa-minus"></i> Eliminar</a></li>
+                   </ul>
+                   </div>';})
+               ->make(true);
     }
 
     /**
@@ -50,7 +57,9 @@ class EconomicComplementProcedureController extends Controller
         $lagging_end_date = Carbon::parse($economic_complement_procedure->lagging_end_date)->format('d/m/Y');
         $additional_start_date = Carbon::parse($economic_complement_procedure->additional_start_date)->format('d/m/Y');
         $additional_end_date = Carbon::parse($economic_complement_procedure->additional_end_date)->format('d/m/Y');
-        return view('economic_complements.procedure.create',compact('normal_start_date','normal_end_date','lagging_start_date','lagging_end_date','additional_start_date','additional_end_date'));
+        $year = Carbon::now()->year;
+        $semester = Util::getCurrentSemester();
+        return view('economic_complements.procedure.create',compact('normal_start_date','normal_end_date','lagging_start_date','lagging_end_date','additional_start_date','additional_end_date','year','semester'));
     }
 
     /**
@@ -59,19 +68,26 @@ class EconomicComplementProcedureController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function save($request, $eco_com_pro=false)
+    public function save($request, $eco_com_pro_id)
     {
-       
-            $eco_com_pro = EconomicComplementProcedure::where('year','=',Util::datePickYear(Carbon::now()->year))->where('semester','=',Util::getCurrentSemester())->first();
-            if ($eco_com_pro) {
-                $message = "Rango de fechas Actualizado";
-            }else {
-                $eco_com_pro = new EconomicComplementProcedure();
-                $message = "Rango de Fechas Creado con éxito";
-            }
-            $eco_com_pro->year = Util::datePickYear(Carbon::now()->year);
+        if ($eco_com_pro_id) {
+            $message = "Rango de fechas Actualizado";
+            $eco_com_pro = EconomicComplementProcedure::find($eco_com_pro_id);
+        }else{
+            $message = "Rango de Fechas Creado con éxito";
+            $eco_com_pro = new EconomicComplementProcedure();
+        }
+            //$eco_com_pro = EconomicComplementProcedure::where('year','=',Util::datePickYear(Carbon::now()->year))->where('semester','=',Util::getCurrentSemester())->first();
+            // if ($eco_com_pro) {
+            //     $message = "Rango de fechas Actualizado";
+            // }else {
+            //     $message = "Rango de Fechas Creado con éxito";
+            // }
+            // 
+            $eco_com_pro->year = Util::datePickYear($request->year);
+            // $eco_com_pro->year = Util::datePickYear(Carbon::now()->year);
             $eco_com_pro->user_id = Auth::user()->id;
-            $eco_com_pro->semester = Util::getCurrentSemester();
+            $eco_com_pro->semester = $request->semester;
             $eco_com_pro->normal_start_date = Util::datePick($request->normal_start_date);
             $eco_com_pro->normal_end_date = Util::datePick($request->normal_end_date);
             $eco_com_pro->lagging_start_date = Util::datePick($request->lagging_start_date);
@@ -85,7 +101,11 @@ class EconomicComplementProcedureController extends Controller
     }
     public function store(Request $request)
     {
-        return $this->save($request);
+        if ($request->economic_complement_procedure_id) {
+            return $this->save($request, $request->economic_complement_procedure_id);
+        }else{
+            return $this->save($request, null);
+        }
     }
 
     /**
@@ -96,7 +116,15 @@ class EconomicComplementProcedureController extends Controller
      */
     public function show($id)
     {
-        //
+        $procedure=EconomicComplementProcedure::find($id);
+        $procedure->normal_start_date=Carbon::parse($procedure->normal_start_date)->format('d/m/Y');
+        $procedure->normal_end_date=Carbon::parse($procedure->normal_end_date)->format('d/m/Y');
+        $procedure->lagging_start_date = Carbon::parse($procedure->lagging_start_date)->format('d/m/Y');
+        $procedure->lagging_end_date = Carbon::parse($procedure->lagging_end_date)->format('d/m/Y');
+        $procedure->additional_start_date = Carbon::parse($procedure->additional_start_date)->format('d/m/Y');
+        $procedure->additional_end_date = Carbon::parse($procedure->additional_end_date)->format('d/m/Y');
+        $procedure->year = Carbon::parse($procedure->year)->year;
+        return $procedure;
     }
 
     /**
