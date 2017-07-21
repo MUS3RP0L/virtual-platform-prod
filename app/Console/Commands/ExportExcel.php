230b2868ecd00e9a15326e8c1ab63384fe91d43b
DESKTOP-6J6U09Q\David <ltorrezs2008@gmail.com>
2017-07-21 17:49:15 -0400

    excel

--- app/Console/Commands/ExportExcel.php
+++ app/Console/Commands/ExportExcel.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Facades\Excel;
 use Muserpol\Affiliate;
 use Muserpol\Contribution;
 use Log;
+use Illuminate\Support\Facades\DB;
 
 class ExportExcel extends Command
 {
@@ -42,22 +43,121 @@ class ExportExcel extends Command
     public function handle()
     {
         //
-        global $rows;
+        global $rows,$afiliadoConflicto;
+       
 
         $this->info("Ejecutando Exportacion");
             
 
            Excel::load('public/file_to_import/PARA LLENADO DE DATOS.xlsx', function($reader) {
 
-                   // global $results, $FileName;
-           global $rows;
-                 //$results = $reader->all();
-            
-                        
-                    // Log::info($reader->toArray());
+                
+           global $rows,$afiliadoConflicto;
 
-                    // $results = collect($reader->select(array('ci', 'ci_a'))->get());
+                     $afiliadoConflicto = array();
+                    
+
+                    //para el primer informe
+                     /*
                     $rows = array();
+                    $titulos= array('ci','Primer Nombre','Segundo Nombre','Apellido Paterno','Apellido Materno','Fecha','Sueldo Base','Categoria','cotizable','cotizable2','FR');
+                    array_push($rows, $titulos);
+                    $results = $reader->select(array('ci', 'ci_a'))->get();
+
+                    
+                    // $grupo = $results->get()->groupBy('ci_a');
+                    
+                    $sheet = $results[0];
+                    foreach ($sheet as $r) {
+
+                        # code...
+                         $afiliado = Affiliate::where('identity_card','=',$r->ci_a)->first();
+                            
+
+                       //  Log::info($afiliado);
+
+                         if($afiliado)
+                         {  
+                            $contribuciones = Contribution::where('affiliate_id','=',$afiliado->id)->orderBy('month_year','ASC')->get();
+
+                            if(sizeof( $contribuciones)>0){
+
+
+                                 $fecha = Contribution::where('affiliate_id','=',$afiliado->id)->orderBy('month_year','ASC')->first();
+                                 if($fecha)
+                                 {
+                                    $fecha_inicio = $fecha->month_year;   
+                                 }
+                                 else
+                                 {
+                                    $fecha_inicio = '0000/00/00';
+                                    array_push($afiliadoConflicto, $afiliado);
+
+                                 }
+                                 
+                                // array_push($afiliadoConflicto, $afiliado);
+                                    
+                                 Log::info($fecha_inicio);
+
+                                $fecha_d = DB::table('affiliate_records')
+                                                ->join('affiliates','affiliate_records.affiliate_id','=','affiliates.id')
+                                                ->where('affiliate_records.affiliate_state_id','=',3)
+                                                ->where('affiliates.identity_card','=',$afiliado->identity_card)->orderBy('affiliate_records.date','ASC')->first();
+                                
+                                if($fecha_d)
+                                {
+
+                                    $fecha_fin =$fecha_d->date;
+                                }
+                                else{
+
+                                    $fecha = Contribution::where('affiliate_id','=',$afiliado->id)->orderBy('month_year','DESC')->first();
+
+                                    if($fecha){
+                                        $fecha_fin =$fecha->month_year;
+                                    }
+                                    else
+                                    {
+                                         $fecha_fin ='0000/00/00';
+                                    }
+                                   
+                                    Log::info("no tiene disponibilidad");
+                                }
+                                      
+                                
+                                Log::info($fecha_fin);
+
+                                $contribuciones = Contribution::where('affiliate_id','=' ,$afiliado->id)->orderBy('month_year','DESC')->select('month_year','base_wage','seniority_bonus','quotable','dignity_pension','study_bonus','position_bonus','border_bonus','east_bonus','public_security_bonus','retirement_fund')->whereBetween('month_year',array($fecha_inicio,$fecha_fin))->take(60)->get();
+
+                                 foreach ($contribuciones as $contribucion) {
+                                     # code...
+                                    $cotizable2 = $contribucion->base_wage + $contribucion->dignity_pension + $contribucion->seniority_bonus + $contribucion->study_bonus + $contribucion->position_bonus + $contribucion->border_bonus + $contribucion->east_bonus - $contribucion->public_security_bonus; 
+                                    $row = array($afiliado->identity_card,$afiliado->first_name,$afiliado->second_name,$afiliado->last_name,$afiliado->mothers_last_name, $contribucion->month_year ,$contribucion->base_wage,$contribucion->seniority_bonus,$contribucion->quotable,$cotizable2,$contribucion->retirement_fund);
+                                    //og::info($contribucion->"");
+                                    array_push($rows, $row);
+                                 }
+
+                            }else
+                            {
+                                $row = array($r->ci_a,"no tiene contribuciones ","","","","" ,"","","","","");
+                                array_push($rows, $row);
+                            }
+                             
+                         }
+                         else
+                         {
+                            $row = array($r->ci_a,"no esta registrado","","","","" ,"","","","","");
+                             array_push($rows, $row);
+                         }
+                        
+    
+                    }
+
+                    */
+
+            //*individual*/ 
+                    $rows = array();
+                    $titulos= array('ci','Exp','fecha_alta','fecha_baja','fecha disponibilidad','cotizaciones ','años','meses');                    array_push($rows, $titulos);
                     $results = $reader->select(array('ci', 'ci_a'))->get();
                     
                     // $grupo = $results->get()->groupBy('ci_a');
@@ -67,19 +167,80 @@ class ExportExcel extends Command
 
                         # code...
                          $afiliado = Affiliate::where('identity_card','=',$r->ci_a)->first();
-                         Log::info($afiliado);
-                         if($afiliado)
-                         {
-                             $contribuciones = Contribution::where('affiliate_id','=' ,$afiliado->id)->orderBy('month_year','DESC')->select('month_year','retirement_fund','mortuary_quota','total')->take(60)->get();
+                         
 
-                             foreach ($contribuciones as $contribucion) {
-                                 # code...
-                                $row = array($afiliado->identity_card,$afiliado->first_name,$afiliado->second_name,$afiliado->last_name,$afiliado->mothers_last_name, $contribucion->month_year , $contribucion->quotable,$contribucion->total);
-                                //og::info($contribucion->"");
+                        if($afiliado)
+                        {   
+                                Log::info($afiliado->id);
+                                $exp = 'sin registro';
+                                if($afiliado->city_identity_card_id)
+                                {
+                                    $exp = $afiliado->city_identity_card->first_shortened;
+                                }
+                                $fecha = Contribution::where('affiliate_id','=',$afiliado->id)->orderBy('month_year','ASC')->first();
+                                $fecha_alta='sin registro';
+                                if($fecha)
+                                {
+                                    $fecha_alta = $fecha->month_year;
+                                }
+                               
+                                $fecha = Contribution::where('affiliate_id','=',$afiliado->id)->orderBy('month_year','DESC')->first();
+                                 $fecha_baja = 'sin registro';
+                                if($fecha)
+                                {
+                                    $fecha_baja = $fecha->month_year;
+                                }   
+
+                                $fecha_disponibilidad='sin disponibilidad';
+
+                                // $fecha_d =DB::select('select distinct date from affiliate_records ar, affiliates a where a.id = ar.affiliate_id and ar.affiliate_state_id = 3 AND  a.identity_card = '.$afiliado->identity_card.' order by date asc');
+
+                                $fecha_d = DB::table('affiliate_records')
+                                            ->join('affiliates','affiliate_records.affiliate_id','=','affiliates.id')
+                                            ->where('affiliate_records.affiliate_state_id','=',3)
+                                            ->where('affiliates.identity_card','=',$afiliado->identity_card)->distinct()->select('affiliate_records.date')->get();
+
+                                if(sizeof($fecha_d)>0)
+                                {
+                                    $cadena="";
+                                    foreach($fecha_d as $f)
+                                    {
+                                        $cadena = $cadena."|".$f->date;
+                                        // Log::info($fecha_d['']);    
+                                    } 
+                                    $fecha_disponibilidad=$cadena;
+                                }            
+
+                                $qty_cotizaciones = 0;
+
+                                $contribuciones_c =  Contribution::where('affiliate_id','=',$afiliado->id)->where('breakdown_id','=',1)->get();
+                                if($contribuciones_c)
+                                {
+                                    $qty_cotizaciones = $contribuciones_c->count();
+                                }
+
+                                $años_contribuciones = 0;
+
+                                    
+
+                                $contribuciones_tiempo = Contribution::where('affiliate_id','=',$afiliado->id)->where('contributions.month_year','>=','28-08-1999')->get();
+                                Log::info($contribuciones_tiempo->count());
+
+                                $años_contribuciones = (int)($contribuciones_tiempo->count()/12);
+                                $meses_contribuciones =  round((($contribuciones_tiempo->count()/12) - $años_contribuciones)*12);
+                                Log::info("años ".$años_contribuciones);
+                                Log::info("meses ".$meses_contribuciones);
+                                $row =array($afiliado->identity_card,$exp,$fecha_alta,$fecha_baja,$fecha_disponibilidad,$qty_cotizaciones,$años_contribuciones,$meses_contribuciones);
                                 array_push($rows, $row);
-                             }
-                         }
-                        
+                        }
+                        else
+                        {
+                                Log::info('sin registro');
+                                 $row =array($r->ci_a,'no se encuentra registrado','-----','----','-----','-----','-----','-----');
+                                array_push($rows, $row);
+                        }
+                
+                         
     
                     }
 
@@ -206,8 +367,13 @@ class ExportExcel extends Command
 
         // })->store('xls', storage_path('excel/exports'));
 
-            
+             $this->info('total exportadors '.sizeof($rows));
+            foreach ($afiliadoConflicto as $af) {
 
-        $this->info('total exportadors '.sizeof($rows));
+                # code...
+                $this->info(' afiliado '.$af->id);
+            }
+
+        
     }
 }
