b17bf213842ce79a0ee1183f14e3d2629c5fd20e
DESKTOP-4AJOACO\Felix <felixbvf@gmail.com>
2017-08-03 15:31:18 -0400

    add Column calculateDate

--- database/migrations/2017_08_03_152924_AddColumnCalculateDate.php
+++ database/migrations/2017_08_03_152924_AddColumnCalculateDate.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnTypeCalculate extends Migration
+class AddColumnCalculateDate extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class AddColumnTypeCalculate extends Migration
      */
     public function up()
     {
-        Schema::table('economic_complements', function(Blueprint $table)
+         Schema::table('economic_complements', function(Blueprint $table)
         {
-            $table->enum('calculation_type', ['NoCalculado', 'Automatico', 'Manual'])->required()->default('NoCalculado');
-                        
+            
+            $table->date('calculation_date')->nullable();            
 
 
         });
