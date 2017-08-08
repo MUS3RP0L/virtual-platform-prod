84d3856d0620cb0a18d2ed70bc6c75dc22553508
DESKTOP-6J6U09Q\David <ltorrezs2008@gmail.com>
2017-08-08 09:51:39 -0400

    bug fixed

--- app/Http/Controllers/EconomicComplement/EconomicComplementController.php
+++ app/Http/Controllers/EconomicComplement/EconomicComplementController.php
@@ -1272,7 +1272,7 @@ class EconomicComplementController extends Controller
                            if (!$eco_com_submitted_document) {
                                 $eco_com_submitted_document = new EconomicComplementSubmittedDocument;
                                 $eco_com_submitted_document->economic_complement_id = $economic_complement->id;
-                                $eco_com_submitted_document->eco_com_requirement_id = 1;
+                                $eco_com_submitted_document->eco_com_requirement_id = $item->id;
                                 $eco_com_submitted_document->status = $item->status;
                                 $eco_com_submitted_document->reception_date = date('Y-m-d');
                                 $eco_com_submitted_document->save();
