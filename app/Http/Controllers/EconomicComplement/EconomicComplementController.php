750f1e2b55f7673725549d3052be4d7fdc7cec77
cechus <cechuscsc4@gmail.com>
2017-08-09 18:28:00 -0400

    bug fixes on econ com requirements ar

--- app/Http/Controllers/EconomicComplement/EconomicComplementController.php
+++ app/Http/Controllers/EconomicComplement/EconomicComplementController.php
@@ -701,7 +701,7 @@ class EconomicComplementController extends Controller
         $economic_complement_legal_guardian=$economic_complement->economic_complement_legal_guardian;
         $affi_observations = AffiliateObservation::where('affiliate_id',$affiliate->id)->first();
         if (EconomicComplement::where('affiliate_id', $affiliate->id)->whereYear('year','=', 2016)->where('semester','=', 'Segundo')->first()) {
-               $last_ecocom = EconomicComplement::where('affiliate_id', $affiliate->id)->whereYear('year','=', 2016)->where('semester','=', 'Segundo')->first();
+               $last_ecocom = EconomicComplement::where('affiliate_id', $affiliate->id)->whereYear('year','=', 2016)->get()->last();
 
                if (EconomicComplementSubmittedDocument::economicComplementIs($last_ecocom->id)->first()) {
                    if ($last_ecocom->economic_complement_modality->economic_complement_type->name == 'Vejez') {
