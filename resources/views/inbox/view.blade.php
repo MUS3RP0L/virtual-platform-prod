c7cb83b0f663b527fda95870c0da29fda0589355
cechus <cechuscsc4@gmail.com>
2017-09-20 12:05:33 -0400

    added button send all

--- resources/views/inbox/view.blade.php
+++ resources/views/inbox/view.blade.php
@@ -38,7 +38,41 @@
 <div class="col-md-6">
 	<div class="box box-success">
 		<div class="box-header with-border">
-			<h3 class="box-title">Recibidos</h3>
+      <div class="col-md-9">
+          <h3 class="box-title">Recibidos</h3>
+      </div>
+      @can('eco_com_approval')
+      <div class="col-md-3">
+        <span data-toggle="modal" data-target="#sendAllModal" >
+          <a href="#" class="btn btn-md btn-raised btn-success" data-toggle="tooltip" data-placement="top" title="Enviar todos los tramites" ><i class="fa  fa-2x fa-arrow-circle-o-right"></i></a>
+        </span>
+      </div>
+      <!-- Modal -->
+      <div class="modal fade" id="sendAllModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog " role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel"><strong>¿Esta seguro de enviar Todos los tramites?</strong></h4>
+            </div>
+            <div class="row text-center">
+              {!! Form::open(['method' => 'POST', 'route' => ['inbox_send_all'], 'class' => 'form-horizontal','id'=>'frm-edited']) !!}
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <a href="#" data-dismiss="modal" class="btn btn-raised btn-warning">&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-remove"></span>&nbsp;&nbsp;&nbsp;</a>
+                        &nbsp;&nbsp;&nbsp;
+                        <button type="submit" class="btn btn-raised btn-success" data-toggle="tooltip" data-placement="bottom" data-original-title="Confirmar">&nbsp;<span class="fa fa-check"></span>&nbsp;</button>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+            <div class="modal-footer">
+            </div>
+          </div>
+        </div>
+      </div>
+       @endcan
+
 		</div>
     <div class="box-body">
   		<table id="received" class="table table-bordered table-hover">
@@ -107,6 +141,8 @@
 	</div>
 </div>
 </div>
+
+
 @push('scripts')
 <script>
 $(document).ready(function (){
