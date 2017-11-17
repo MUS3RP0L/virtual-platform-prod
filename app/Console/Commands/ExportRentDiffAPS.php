6060e86d2402ee16576a4c1b767a7659bc302bf8
cechus <cechuscsc4@gmail.com>
2017-11-17 18:54:01 -0400

    reports

--- app/Console/Commands/ExportRentDiffAPS.php
+++ app/Console/Commands/ExportRentDiffAPS.php
@@ -5,14 +5,20 @@ namespace Muserpol\Console\Commands;
 use Illuminate\Console\Command;
 use Muserpol\EconomicComplement;
 
-class ExportRentDiff extends Command
+use Muserpol\Helper\Util;
+use Maatwebsite\Excel\Facades\Excel;
+use Carbon\Carbon;
+use DB;
+
+
+class ExportRentDiffAPS extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'export:diff_rent';
+    protected $signature = 'export:diff_rent_aps';
 
     /**
      * The console command description.
@@ -38,6 +44,7 @@ class ExportRentDiff extends Command
      */
     public function handle()
     {
+        global $diffs;
         $count=0;
         $diff=0;
         $eco_coms=EconomicComplement::where('eco_com_procedure_id','=', 6)->leftJoin('affiliates', 'economic_complements.affiliate_id','=', 'affiliates.id')
@@ -52,10 +59,18 @@ class ExportRentDiff extends Command
                 $s=floatval($eco->aps_total_cc) + floatval($eco->aps_total_fs) + floatval($eco->aps_total_fsa);
                 if (floatval($eco_old->total_rent).'' <>  floatval($s).'') {
                     $diff++;
-                    // dd($eco->id,floatval($eco_old->total_rent).'', floatval($s).'');
+                    $diffs[]=$eco->id;
                 }
             }
         }
-        dd($count, $diff);
+        $columns = '';
+        $economic_complements=EconomicComplement::whereIn('economic_complements.id', $diffs)
+        ->ecocominfo()
+        ->applicantinfo()
+        ->affiliateinfo()
+        ->select(DB::raw(EconomicComplement::basic_info_colums()."".$columns.""))
+        ->get();
+        $data = $economic_complements;
+        Util::excelSave('Lista Afiliados que varian las rentas con APS', 'hoja', $data, 'excel/exports');
     }
 }
