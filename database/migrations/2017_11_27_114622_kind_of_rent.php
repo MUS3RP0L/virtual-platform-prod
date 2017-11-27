90374ec5d295d37ecb3c557b2e5cca69f1eccf38
Ale Guis F <aleguis@icloud.com>
2017-11-27 16:13:02 -0400

    added kind rent on eco com

---
+++ database/migrations/2017_11_27_114622_kind_of_rent.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class KindOfRent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eco_com_kind_rent', function(Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+        });
+
+        Schema::table('economic_complements', function (Blueprint $table) {
+             $table->bigInteger('eco_com_kind_rent_id')->nullable();
+             $table->foreign('eco_com_kind_rent_id')->references('id')->on('eco_com_kind_rent');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('degrees');
+    }
+}
