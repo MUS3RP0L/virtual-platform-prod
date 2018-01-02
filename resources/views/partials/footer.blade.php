d7b79ca54111417376c691127c1fa963df231885
alerick <aleguis@icloud.com>
2018-01-02 10:19:20 -0400

    bug fixes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <div class="text-right">
-        <strong>Plataforma Virtual de la Mutual de Servicios al Policía</strong> - 2017
+        <strong>Plataforma Virtual de la Mutual de Servicios al Policía</strong> - 2018
     </div>
 </footer>
