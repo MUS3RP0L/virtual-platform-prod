a12b3b10e7267893b9afcbaad0618536215817c8
cechus <cechuscsc4@gmail.com>
2018-09-03 21:16:10 -0400

    added report bank (two)

--- resources/views/economic_complements/index.blade.php
+++ resources/views/economic_complements/index.blade.php
@@ -107,6 +107,8 @@
                 <ul class="dropdown-menu">
                     <li><a href="" data-toggle="modal" data-target="#myModal-exportbanco" style="padding:3px 5px;"><i class="fa fa-money"></i>Banco</a></li>
                     <li role="separator" class="divider"></li>
+                    <li><a href="" data-toggle="modal" data-target="#myModal-exportbanco-2" style="padding:3px 5px;"><i class="fa fa-money"></i>Banco 2</a></li>
+                    <li role="separator" class="divider"></li>
                     {{-- <li><a href="{{url('export_planilla_general_bank')}}"  style="padding:3px 5px;"><i class="fa  fa-file-excel-o"></i>Planilla General Banco</a></li>  
                      <li role="separator" class="divider"></li>
                     <li><a href="" data-toggle="modal" data-target="#myModal-exporbydepartment-bank" style="padding:3px 5px;"><i class="fa  fa-file-excel-o"></i>Planilla por Departamento</a></li>
@@ -452,6 +454,60 @@
                         <div class="modal-body">
 
                             {!! Form::open(['method' => 'POST', 'route' => ['export_bank'], 'class' => 'form-horizontal', 'files' => true ]) !!}
+
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                                {!! Form::label('year', 'Año', ['class' => 'col-md-3 control-label']) !!}
+                                            <div class="col-md-7">
+                                                <div class="input-group">
+                                                      {!! Form::text('year', $year, ['class'=> 'form-control', 'required' => 'required']) !!}
+                                                      <span class="help-block">Año</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                                {!! Form::label('semestre', 'Semestre', ['class' => 'col-md-3 control-label']) !!}
+                                            <div class="col-md-7">
+                                                <div class="form-group">
+                                                      {!! Form::select('semester',$semester_list,'',['class' => 'combobox form-control', 'required' => 'required']) !!}
+                                                      <span class="help-block">Seleccione Semestre</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row text-center">
+                                    <div class="form-group">
+                                        <div class="col-md-12">
+                                            <a href="{!! url('economic_complement') !!}" class="btn btn-raised btn-warning" data-toggle="tooltip" data-placement="bottom" data-original-title="Cancelar">&nbsp;<i class="glyphicon glyphicon-remove"></i>&nbsp;</a>
+                                            &nbsp;&nbsp;
+                                            <button type="submit" class="btn btn-raised btn-success" data-toggle="tooltip" data-placement="bottom" data-original-title="Guardar">&nbsp;<i class="glyphicon glyphicon-floppy-disk"></i>&nbsp;</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+    </div>
+    {{-- EXPORT FOR BANK 2 --}}
+    <div id="myModal-exportbanco-2" class="modal fade bs-example-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="box-header with-border">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Exportar Afiliados de Complemento Económico - BANCO 2 </h4>
+                        </div>
+                        <div class="modal-body">
+
+                            {!! Form::open(['method' => 'POST', 'route' => ['export_bank_two'], 'class' => 'form-horizontal', 'files' => true ]) !!}
 
                                 <br>
                                 <div class="row">
