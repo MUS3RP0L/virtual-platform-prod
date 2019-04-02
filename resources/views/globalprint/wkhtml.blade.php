40b0b72add645cd012f8d1f2be49a94bfc8a1daf
alerick <aleguis@icloud.com>
2019-04-02 11:19:03 -0400

    print last procedure

--- resources/views/globalprint/wkhtml.blade.php
+++ resources/views/globalprint/wkhtml.blade.php
@@ -40,7 +40,7 @@
     <table >
       <tr>
         <td class="izq no-border size-10">
-         <strong>Fecha de Emisi&#243n: </strong> 24 de marzo de 2019 - 18:30:33
+         <strong>Fecha de Emisi&#243n: </strong>{!! $date !!} - {!! $hour !!}
         </td>
         <td class="der no-border size-10">
           @if(isset($user))
