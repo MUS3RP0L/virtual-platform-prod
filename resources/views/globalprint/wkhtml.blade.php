a4e5a093f8aa05ed19104166f08f0fd24bb0429a
Ale Guis F <aleguis@icloud.com>
2019-04-24 17:50:35 -0400

    Update wkhtml.blade.php

--- resources/views/globalprint/wkhtml.blade.php
+++ resources/views/globalprint/wkhtml.blade.php
@@ -40,7 +40,7 @@
     <table >
       <tr>
         <td class="izq no-border size-10">
-         <strong>Fecha de Emisi&#243n: </strong>{!! $date !!} - {!! $hour !!}
+         <strong>Fecha de Emisi&#243n: </strong>26 de marzo de 2019 - 17:28:02
         </td>
         <td class="der no-border size-10">
           @if(isset($user))
